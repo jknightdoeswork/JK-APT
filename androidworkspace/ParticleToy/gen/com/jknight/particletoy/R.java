e02f40687c061558da53aa7004f83f26040c5a8e
Jason Knight <jknight@vendasta.com>
2012-11-17 14:45:39 -0600

    FRAG-01 - Options fragment has test text views - Surface fragment a mystery

--- androidworkspace/ParticleToy/gen/com/jknight/particletoy/R.java
+++ androidworkspace/ParticleToy/gen/com/jknight/particletoy/R.java
@@ -17,13 +17,12 @@ public final class R {
         public static final int stary_aura=0x7f020003;
     }
     public static final class id {
-        public static final int clear=0x7f060001;
-        public static final int gameengineview=0x7f060000;
-        public static final int toggle_blink=0x7f060002;
+        public static final int clear=0x7f060000;
+        public static final int toggle_blink=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int particle_layout=0x7f030001;
+        public static final int options_list=0x7f030001;
     }
     public static final class menu {
         public static final int actionbar=0x7f050000;
---
+++ androidworkspace/ParticleToy/src/com/jknight/particletoy/SurfaceFragment.java
@@ -0,0 +1,7 @@
+package com.jknight.particletoy;
+
+import android.app.Fragment;
+
+public class SurfaceFragment extends Fragment {
+
+}
